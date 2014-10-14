c428831bff64c8651afd9a9e041bec419206269d
Gashaw Mola <gashaw.mola@gmail.com>
2014-10-14 18:29:06 +0300

    droped phantom-manager, using god instead

---
+++ config/god/phantomjs.rb
@@ -0,0 +1,54 @@
+env   = ENV['ENV'] || 'development'
+project_root  = ENV['ROOT'] || "/home/deploy/readify/current"
+
+%w{8001 8002}.each do |port|
+  God.watch do |w|
+    w.dir      = project_root
+    w.name     = "phantomjs-#{ port }"
+    w.start    = "phantomjs server.js"
+    w.log      = "#{project_root}/log/phantomjs-#{ port }.std.log"
+    w.err_log  = "#{project_root}/log/phantomjs-#{ port }.err.log"
+    w.env = { 'PORT' => "#{ port }" }
+
+    # determine the state on startup
+    w.transition(:init, { true => :up, false => :start }) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+      end
+    end
+
+    # determine when process has finished starting
+    w.transition([:start, :restart], :up) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+      end
+
+      # failsafe
+      on.condition(:tries) do |c|
+        c.times = 5
+        c.transition = :start
+      end
+    end
+
+    # start if process is not running
+    w.transition(:up, :start) do |on|
+      on.condition(:process_exits)
+    end
+
+    # restart if memory or cpu is too high
+    w.transition(:up, :restart) do |on|
+      on.condition(:memory_usage) do |c|
+        c.interval = 20
+        c.above = 100.megabytes
+        c.times = [3, 5]
+      end
+
+      on.condition(:cpu_usage) do |c|
+        c.interval = 10
+        c.above = 30.percent
+        c.times = [3, 5]
+      end
+    end
+
+  end
+end
