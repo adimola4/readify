00f900a704e8c2be27795769a4d6cbe40ff11e73
Gashaw Mola <gashaw.mola@gmail.com>
2014-10-14 19:43:07 +0300

    added watch for nginx

--- config/god/phantomjs.rb
+++ config/god/phantomjs.rb
@@ -63,3 +63,40 @@ project_root  = ENV['ROOT'] || "/home/deploy/readify/current"
 
   end
 end
+
+God.watch do |w|
+  w.name = "nginx"
+  w.interval = 30.seconds
+  w.start = "nginx -c #{ project_root }/config/nginx/#{ env }.conf"
+  w.stop = "nginx -s stop"
+  w.start_grace = 20.seconds
+  w.pid_file = "/run/nginx.pid"
+    
+  w.behavior(:clean_pid_file)
+
+  # determine the state on startup
+  w.transition(:init, { true => :up, false => :start }) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+    end
+  end
+
+  # determine when process has finished starting
+  w.transition([:start, :restart], :up) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+    end  
+    
+    # failsafe
+    on.condition(:tries) do |c|
+      c.times = 5
+      c.transition = :start
+    end
+  end
+
+  # start if process is not running
+  w.transition(:up, :start) do |on|
+    on.condition(:process_exits)
+  end
+
+end
